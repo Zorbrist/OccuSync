95f63d6bbe410a334544b35ae5154b3e4ae9feca
Zorbrist <145312660+Zorbrist@users.noreply.github.com>
2026-09-07 19:04:26 +0800

    added register customer and register business routes

--- frontend-OccuSync/src/App.tsx
+++ frontend-OccuSync/src/App.tsx
@@ -1,7 +1,8 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
-import Register from './pages/Register'
+import RegisterBusiness from './pages/RegisterBusiness';
+import RegisterCustomer from './pages/RegisterCustomer';
 import AboutUs from './pages/AboutUs';
 import './App.css';
 
@@ -12,7 +13,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs/>} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/register/business" element={<RegisterBusiness />} />
+        <Route path="/register/customer" element={<RegisterCustomer />} />
       </Routes>
     </Router>
   )

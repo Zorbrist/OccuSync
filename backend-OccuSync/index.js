1db5a1ddb6ad9f5d89316381c41a3dce9d72ddaf
Zorbrist <145312660+Zorbrist@users.noreply.github.com>
2026-09-07 11:39:38 +0800

    OC-14: registration api

--- backend-OccuSync/index.js
+++ backend-OccuSync/index.js
@@ -1,12 +1,18 @@
+require('dotenv').config();
 const express = require("express");
-const app = express()
-const PORT = 3000
+const cors = require("cors")
+const authRoutes = require('./src/routes/authRoutes')
+const app = express();
+const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
-});
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
+
+app.use('/auth', authRoutes)
+
 
 app.listen(PORT, () => {
-  console.log("Server running at http://localhost:3000");
+    console.log(`Server running on port ${PORT}`);
 });
-
